466ae98e8fcd0625d590cd9adb1e4ce97b72432a
Seongwon Han <seongwonhan88@gmail.com>
2020-04-02 23:39:29 -0400

    passing env variable through docker compose

--- personal/models.py
+++ personal/models.py
@@ -7,7 +7,7 @@ class ContactInfo(models.Model):
     name = models.CharField('Name', max_length=150, blank=False, null=False)
     email = models.EmailField('Email address', blank=False, null=False)
     message_type = models.CharField('What is this regarding?', max_length=128, choices=CHOICES, blank=False, null=False)
-    message_content = models.TextField('Message', blank=False, null=False)
+    message_content = models.TextField('Message details', blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- portfolio/settings/__init__.py
+++ portfolio/settings/__init__.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('PORTFOLIO_DEFAULT_DB_NAME', 'portfolio'),
+        'USER': os.environ.get('PORTFOLIO_DEFAULT_DB_USER', 'portfolio'),
+        'PASSWORD': os.environ.get('PORTFOLIO_DEFAULT_DB_PASSWORD', 'portfolio'),
+        'HOST': os.environ.get('PORTFOLIO_DEFAULT_DB_USER', 'portfolio'),
+        'PORT': 5432
     }
 }
 
