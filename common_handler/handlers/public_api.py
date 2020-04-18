dc27e1aae261b5d0179764b64ba31c8d22231795
Seongwon Han <seongwonhan88@gmail.com>
2020-04-17 23:35:24 -0400

    api updated

--- common_handler/handlers/public_api.py
+++ common_handler/handlers/public_api.py
@@ -9,5 +9,5 @@ def covid19_middleware_api(group=None):
     base_api = 'https://corona.lmao.ninja'
     if not group:
         group = 'all'
-    response = requests.get(f'{base_api}/{group}')
+    response = requests.get(f'{base_api}/v2/{group}')
     return response
